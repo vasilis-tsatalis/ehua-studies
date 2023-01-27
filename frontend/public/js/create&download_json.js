a93a575a97aeb4e275f62e3e57e9e2c5b1f81e9a
vasilis-tsatalis <tsatalis.vas@gmail.com>
2023-01-28 01:47:31 +0200

    Fix export lists errors

--- frontend/public/js/create&download_json.js
+++ frontend/public/js/create&download_json.js
@@ -1,24 +1,27 @@
 ////////////////////////////////////////////////////////
-function tableToJSON() {
-
-  const table = document.querySelector('table')
-  const [header] = table.tHead.rows
-  const props = [...header.cells].map(h => h.textContent)
-  const rows = [...table.rows].map(r => {
-    const entries = [...r.cells].map((c, i) => {
-      return [props[i], c.textContent]
-    })
-    return Object.fromEntries(entries)
-  })
-  console.log(rows)
-
-}
-////////////////////////////////////////////////////////
 function downloadObjectAsJson(exportObj, exportName){
-    var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
+
+    var table = document.getElementById(exportObj);
+    var header = [];
+    var rows = [];
+
+    for (var i = 0; i < table.rows[0].cells.length; i++) {
+        header.push(table.rows[0].cells[i].innerHTML);
+    }
+
+    for (var i = 1; i < table.rows.length; i++) {
+        var row = {};
+        for (var j = 0; j < table.rows[i].cells.length; j++) {
+            row[header[j]] = table.rows[i].cells[j].innerHTML;
+        }
+        rows.push(row);
+    }
+
+
+    var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(rows));
     var downloadAnchorNode = document.createElement('a');
     downloadAnchorNode.setAttribute("href", dataStr);
-    downloadAnchorNode.setAttribute("download", 'exportName' + ".json");
+    downloadAnchorNode.setAttribute("download", 'file' + ".json");
     document.body.appendChild(downloadAnchorNode); // required for firefox
     downloadAnchorNode.click();
     downloadAnchorNode.remove();
